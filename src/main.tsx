93281612dd1f9986a882ec177a10ab5e0d4ca88d
Denis Ladeira <105209483+denisfloyd@users.noreply.github.com>
2023-03-20 20:55:30 -0300

    chore: :building_construction: add a pre defined template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from 'App';
+import GlobalStyles from 'lib/GlobalStyles';
+import RecoilProvider from 'components/providers/RecoilProvider';
+import QueryProvider from 'components/providers/QueryProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryProvider>
+      <RecoilProvider>
+        <GlobalStyles />
+        <App />
+      </RecoilProvider>
+    </QueryProvider>
   </React.StrictMode>,
-)
+);
